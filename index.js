1c5eba185d40f0adfef3aa2195221b4686c64a61
Justus <galaxycheesemc@gmail.com>
2023-01-28 12:31:28 -0700

    Added Manager Creator

--- index.js
+++ index.js
@@ -1,109 +1,111 @@
-const pageTemplate = require("./src/page-template.js")
 const path = require("path");
 const inquirer = require("inquirer");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const fs = require("fs");
+const pageTemplate = require("./src/page-template.js")
 
 const DIST_DIR = path.resolve(__dirname, "dist");
 const distPath = path.join(DIST_DIR, "index.html;");
 
 const teamArray = [];
+const idArray = [];
 
 console.log(`This is a team generator. \n use "npm run reset" to reset the generated html file.
 \n Input your team details below`);
 
-const managerArray = [
-    {
-        type: "input",
-        name: "managerName",
-        message: "What is the team manager's first name?"
-    },
-    {
-        type: "input",
-        name: "managerId",
-        message: "What is the team manager's id?"
-    },
-    {
-        type: "input",
-        name: "managerEmail",
-        message: "What is the team manager's email?"
-    }, 
-    {
-        type: "input",
-        name: "managerOffice",
-        message: "What is the team manager's office number"
-    }, 
-    //Create inquiries that continue to run until none is inputted
-    {
-        type: "list",
-        name: "typeMember",
-        message: "Which type of team member do you wish to add?",
-        choices: ["Employee","Engineer","Intern","None"]
-    },
-    {
-        type: "input",
-        name: "contributors",
-        message: "Who are the contributors of your project?"
-    },
-    {
-        type: "input",
-        name: "installation",
-        message: "What are the steps required to install your project if applicable?"
-    },
-    {
-        type: "input",
-        name: "usage",
-        message: "Provide instructions and examples for use here."
-    },
-    {
-        type: "input",
-        name: "projectImage",
-        message: "Add an image to the image folder then enter the image name here."
-    },    
-    {
-        type: "input",
-        name: "projectVideo",
-        message: "Add a video link to showcase a demo of your project."
-    },  
-    {
-        type: "input",
-        name: "tests",
-        message: "Provide what tests were implemented for the project's success."
-    },
-    {
-        type: "input",
-        name: "github",
-        message: "What is your github username?"
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "What is your email address?"
-        //Create a function validation that uses .includes(@something.com) or find an inquirer thing that does
-
-    },
-    {
-        type: "list",
-        name: "license",
-        message: "What license is placed on this project?",
-        choices: ["Apache","BSD-3","GPL","MIT", "Unlicense", "No License"]
-    }];
-
-// TODO: Create a function to write README file
 function writeToFile(filename, data) {
     fs.writeFile(filename, data, (err) =>
     err ? console.error(err) : console.log("You successfully created a README.md file."))
 };
-// TODO: Create a function to initialize app
-function init(){
-    inquirer.prompt(managerArray)
-    .then(function(response){
-    var myMarkdown = generateMarkdown(response);
-    writeToFile(filename, myMarkdown);
-    });
+
+function appStart(){
+    function makeTeam(){
+        inquirer
+            .prompt([
+                {
+                    type: "list",
+                    name: "typeMember",
+                    message: "Which type of team member do you wish to add?",
+                    choices: ["Manager","Engineer","Intern","None"]
+                }  
+            ])
+            .then((response) => {
+                switch (response.typeMember){
+                    case "Manager": inqManager();
+                        break;
+                    case "Engineer": inqEngineer();
+                        break;
+                    case "Intern": inqIntern();
+                        break;
+                    default: createTeam();
+                }
+            });
+    }
+    const inqManager = () => {
+        inqManager.prompt([
+            {
+                type: "input",
+                name: "managerName",
+                message: "What is the team manager's first name?",
+                validate: (answer) => {
+                    if(answer !== ""){
+                        return true;
+                    }
+                    return "You have to enter a name for the manager."
+                }
+            },
+            {
+                type: "input",
+                name: "managerID",
+                message: "What is the team manager's ID number",
+                validate: (answer) => {
+                    const pass = answer.match(/^[1-9]\d*$/);
+                    if(pass){
+                        if (idArray.includes(answer)){
+                            return "This ID is already in use. Enter a new ID."
+                        } else {
+                            return true;
+                        }
+                    }
+                    return "You have to enter a positive number for the manager's ID."
+                }
+            },
+            {
+                type: "input",
+                name: "managerOffice",
+                message: "What is the team manager's office number",
+                validate: (answer) => {
+                    const pass = answer.match(/^[1-9]\d*$/);
+                    if(pass){
+                        return true;
+                    }
+                    return "You have to enter a positive number for the manager's office."
+                }
+            },
+            {
+                type: "input",
+                name: "managerEmail",
+                message: "What is the team manager's email"
+            }
+
+        ])
+        .then((response) => {
+            const manager = new Manager(
+                response.managerName,
+                response.managerID,
+                response.managerEmail,
+                response.managerOffice
+            );
+            teamArray.push(manager);
+            idArray.push(response.managerID);
+            makeTeam();
+        })
+    }
+
+    makeTeam();
 };
 
-// Function call to initialize app
-init();
+
+appStart();
--- src/page-template.js
+++ src/page-template.js
@@ -1,43 +1,109 @@
-
-function generateHTML(data) {
-
-    return `# ${data.title}
-  
-  ## Description
-  ${data.description}
-  ${data.projectImage}
-  ## 📝 Table of Contents
-  - <a href="#description">Description</a>
-  - <a href="#installation">Installation</a>
-  - <a href="#usage">Usage</a>
-  - <a href="#contributors">Contributors</a>
-  - <a href="#license">License</a>
-  - <a href="#questions">Questions</a>
-  - <a href="#tests">Tests</a>
-  
-  ## Installation
-  ${data.installation}
-  
-  ## Usage
-  ${data.usage}
-  ![Project Image](utils/${data.projectImage})
-  
-  ${data.projectVideo}
-  
-  ## Contributors
-  ${data.contributors}
-  
-  ## Tests
-  ${data.tests}
-  
-  ## Questions
-  Visit https://github.com/${data.github}/ or contact ${data.email} for any questions.
-  
-  ## Link
-  ${data.link}
-  
-  ${licenseSection}
-  `;
+const createTeam = (teamArray){
+  const createManager = (manager) => {
+    return `
+    <div class="col d-flex justify-content-center">
+                <div class="card" style="width: 20rem">
+                    <div class="card-body">
+                        <ul class="list-group">
+                            <li class="list-group-item heading">${manager.getEmployeeName()}</li>
+                            <li class="list-group-item heading">${manager.getRole()}</li>
+                            <li class="list-group-item">${manager.getEmployeeID()}</li>
+                            <li class="list-group-item">${manager.getEmployeeEmail()}</li>
+                            <li class="list-group-item">${manager.getOffice()}</li>
+                        </ul>
+                    </div>
+                </div>
+    `
+  }
+  const createEngineer = (engineer) => {
+    return `
+    <div class="col d-flex justify-content-center">
+                <div class="card" style="width: 20rem">
+                    <div class="card-body">
+                        <ul class="list-group">
+                            <li class="list-group-item heading">${engineer.getEmployeeName()}</li>
+                            <li class="list-group-item heading">${engineer.getRole()}</li>
+                            <li class="list-group-item">${engineer.getEmployeeID()}</li>
+                            <li class="list-group-item">${engineer.getEmployeeEmail()}</li>
+                            <li class="list-group-item">${engineer.getGitHub()}</li>
+                        </ul>
+                    </div>
+                </div>
+    `
+  }
+  const createIntern = (intern) => {
+    return `
+    <div class="col d-flex justify-content-center">
+                <div class="card" style="width: 20rem">
+                    <div class="card-body">
+                        <ul class="list-group">
+                            <li class="list-group-item heading">${intern.getEmployeeName()}</li>
+                            <li class="list-group-item heading">${intern.getRole()}</li>
+                            <li class="list-group-item">${intern.getEmployeeID()}</li>
+                            <li class="list-group-item">${intern.getEmployeeEmail()}</li>
+                            <li class="list-group-item">${intern.getSchool()}</li>
+                        </ul>
+                    </div>
+                </div>
+    `
   }
   
-  module.exports = generateMarkdown;
+  const generatedCards = [];
+  
+  generatedCards.push(
+    teamArray
+      .filter((teamMember) => teamMember.getEmployeeRole() === "Manager")
+      .map((manager) => createManager(manager))
+      .join("")
+  );
+  generatedCards.push(
+    teamArray
+      .filter((teamMember) => teamMember.getEmployeeRole() === "Engineer")
+      .map((engineer) => createEngineer(engineer))
+      .join("")
+  );
+  generatedCards.push(
+    teamArray
+      .filter((teamMember) => teamMember.getEmployeeRole() === "Intern")
+      .map((intern) => createIntern(intern))
+      .join("")
+  );
+
+  return generatedCards.join("");
+};
+
+
+
+
+
+
+
+
+module.exports = (teamArray) => {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link 
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" 
+            rel="stylesheet" 
+            integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" 
+            crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+        <title>Team Creator</title>
+    </head>
+    <body>
+        <h1>Team generator</h1>
+            <div class="row">
+                ${createTeam(teamArray)}
+            </div>
+    
+    
+    
+    </body>
+    </html>
+  `;
+  }
