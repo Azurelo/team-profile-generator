29e7655f3a834a8dccf570a233f31573a1a0d8a9
Justus <galaxycheesemc@gmail.com>
2023-01-28 12:40:15 -0700

    added intern and engineer inquiry

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ function appStart(){
             });
     }
     const inqManager = () => {
-        inqManager.prompt([
+        inquirer.prompt([
             {
                 type: "input",
                 name: "managerName",
@@ -100,6 +100,112 @@ function appStart(){
             );
             teamArray.push(manager);
             idArray.push(response.managerID);
+            makeTeam();
+        })
+    }
+    const inqEngineer = () => {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "engineerName",
+                message: "What is the engineer's first name?",
+                validate: (answer) => {
+                    if(answer !== ""){
+                        return true;
+                    }
+                    return "You have to enter a name for the engineer."
+                }
+            },
+            {
+                type: "input",
+                name: "engineerID",
+                message: "What is the engineer's ID number",
+                validate: (answer) => {
+                    const pass = answer.match(/^[1-9]\d*$/);
+                    if(pass){
+                        if (idArray.includes(answer)){
+                            return "This ID is already in use. Enter a new ID."
+                        } else {
+                            return true;
+                        }
+                    }
+                    return "You have to enter a positive number for the engineer's ID."
+                }
+            },
+            {
+                type: "input",
+                name: "engineerGitHub",
+                message: "What is the engineer's GitHub Username",
+            },
+            {
+                type: "input",
+                name: "engineerEmail",
+                message: "What is the engineer's email"
+            }
+
+        ])
+        .then((response) => {
+            const engineer = new Engineer(
+                response.engineerName,
+                response.engineerID,
+                response.engineerEmail,
+                response.engineerGitHub
+            );
+            teamArray.push(engineer);
+            idArray.push(response.engineerID);
+            makeTeam();
+        })
+    }
+        const inqIntern = () => {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "internName",
+                message: "What is the intern's first name?",
+                validate: (answer) => {
+                    if(answer !== ""){
+                        return true;
+                    }
+                    return "You have to enter a name for the intern."
+                }
+            },
+            {
+                type: "input",
+                name: "internID",
+                message: "What is the intern's ID number",
+                validate: (answer) => {
+                    const pass = answer.match(/^[1-9]\d*$/);
+                    if(pass){
+                        if (idArray.includes(answer)){
+                            return "This ID is already in use. Enter a new ID."
+                        } else {
+                            return true;
+                        }
+                    }
+                    return "You have to enter a positive number for the intern's ID."
+                }
+            },
+            {
+                type: "input",
+                name: "internSchool",
+                message: "What is the intern's school?",
+            },
+            {
+                type: "input",
+                name: "internEmail",
+                message: "What is the intern's email"
+            }
+
+        ])
+        .then((response) => {
+            const intern = new Intern(
+                response.internName,
+                response.internID,
+                response.internEmail,
+                response.internSchool
+            );
+            teamArray.push(intern);
+            idArray.push(response.internID);
             makeTeam();
         })
     }
